3a8b4d37adc8f16949eb9999214731546ded355e
Shanmuga Veerappan <vrssundar08@yahoo.com>
2013-10-23 16:57:01 -0400

    changed Address and course

--- src/AddressTest.java
+++ src/AddressTest.java
@@ -5,24 +5,59 @@ import org.junit.Test;
 
 public class AddressTest {
 
-	@Test
-	public void testAddressStringStringString() {
-		fail("Not yet implemented");
-	}
+	
 
-	@Test
-	public void testAddressIntStringStringString() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		
+		try {
+			Address address = new Address(6,"vale st","ottawa","K2A 2B9");			
+			assertEquals("6 vale st ottawa K2A 2B9", address.toString());
+			
+		} catch (Exception e) {
+			fail("It jus throws an exception");
+		}
+		
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		try {
+			Address address = new Address(6,"vale st","ottawa","K2A 2B9");
+			Address address2 = new Address(6, "dole", "ottawa", "K2H 7G6");
+			assertTrue(address2.equals(address));
+			assertFalse(address2.equals(address));
+		} catch (Exception e) {
+			fail("It jus throws an exception");
+		}
+	}	
+		@Test
+		public void testToString2() {
+			
+			try {
+				Address address = new Address("vale st","ottawa","K2A 2B9");			
+				assertEquals("6, vale st, ottawa, K2A 2B9", address.toString());
+				
+			} catch (Exception e) {
+				fail("It jus throws an exception");
+			}
+			
+		}
+
+		@Test
+		public void testEqualsObject2() {
+			try {
+				Address address = new Address("vale st","ottawa","K2A 2B9");
+				Address address2 = new Address("dole", "ottawa", "K2H 7G6");
+				assertTrue(address2.equals(address));
+				assertFalse(address2.equals(address));
+			} catch (Exception e) {
+				fail("It jus throws an exception");
+			}
+		
 	}
+
+
 
 }
--- src/CourseTest.java
+++ src/CourseTest.java
@@ -5,39 +5,42 @@ import org.junit.Test;
 
 public class CourseTest {
 
-	@Test
-	public void testCourse() {
-		fail("Not yet implemented");
-	}
+
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		assertEquals("1001",course.getId());
+		
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		assertEquals("Ecor",course.getName());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		assertEquals("Ecor 1001",course.toString());
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		Course course2 = new Course("1002", "Ecor");
+		assertTrue(course2.equals(course));
 	}
 
-	@Test
-	public void testSetProfessor() {
-		fail("Not yet implemented");
-	}
+
 
 	@Test
 	public void testGetProfessor() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		Professor prof = new Professor("Shan","Veer");
+		course.setProfessor(prof);
+		assertEquals(prof,course.getProfessor());		
 	}
 
 	@Test
@@ -52,7 +55,24 @@ public class CourseTest {
 
 	@Test
 	public void testGetStudents() {
-		fail("Not yet implemented");
+		Course course = new Course("1001","Ecor");
+		Student student = new Student("James","Ryan");
+		Student student2 = new Student("Korey","Conway");
+		Student student3 = new Student("Jimmy","Zheng");
+		course.addStudent(student3);
+		course.addStudent(student2);
+		course.addStudent(student);
+		course.removeStudent(student3);
+		Student[] Students = course.getStudents();
+		assertNotNull(Students);
+		assertEquals(2, Students.length);
+
+		boolean c1IsInArray = student.equals(Students[0]) || student.equals(Students[1]);
+		assertTrue(c1IsInArray);
+
+		boolean c2IsInArray = student2.equals(Students[0]) || student2.equals(Students[1]);
+		assertTrue(c2IsInArray);
+		
 	}
 
 }
